abf04bb25182ea97081bb2e54a76498967c06b0b
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-09-25 22:14:25 -0500

    testing components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <Home msg="Bleu Fantasy Art"/>
+    <Header />
+    <Home msg="Art"/>
   </div>
 </template>
 
 <script>
 import Home from './components/Home.vue'
+import Header from './components/Header'
 
 export default {
   name: 'app',
   components: {
-    Home
+    Home,
+    Header
   }
 }
 </script>
@@ -23,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
